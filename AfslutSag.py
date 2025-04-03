d37d6b96a77bb49ad16cdf74f1f871ab0fa96ca1
gujc@aarhus.dk <nruXH246>
2025-04-03 14:06:46 +0200

    CaseList

--- AfslutSag.py
+++ AfslutSag.py
@@ -8,7 +8,7 @@ def invoke_AfslutSag(Arguments_AfslutSag,orchestrator_connection: OrchestratorCo
     from datetime import datetime
     # henter in_argumenter:
     Sagsnummer = Arguments_AfslutSag.get("in_Sagsnummer") #Kø-element
-    KMDNovaURL = Arguments_AfslutSag.get("in_KMDNovaURL") #Kø-element
+    KMDNovaURL = Arguments_AfslutSag.get("in_KMDNovaURL") #Credential/contant
     KMD_access_token = Arguments_AfslutSag.get("in_NovaToken") # process 
     DeskProID = Arguments_AfslutSag.get("in_DeskProID") #Kø-element
 
@@ -45,209 +45,210 @@ def invoke_AfslutSag(Arguments_AfslutSag,orchestrator_connection: OrchestratorCo
     
     CaseUuid = fetch_case_uuids_by_deskpro(DeskProID)
     
-    # Looper igennem caseUuid'erne:
-    for case_uuid in CaseUuid:
-        
-        ## --- Henter CaseTitle --- #
-        TransactionID = str(uuid.uuid4())
+    if CaseUuid:
+        # Looper igennem caseUuid'erne:
+        for case_uuid in CaseUuid:
+            
+            ## --- Henter CaseTitle --- #
+            TransactionID = str(uuid.uuid4())
 
-        Caseurl = f"{KMDNovaURL}/Case/GetList?api-version=2.0-Case"
+            Caseurl = f"{KMDNovaURL}/Case/GetList?api-version=2.0-Case"
 
-        # Define headers
-        headers = {
-            "Authorization": f"Bearer {KMD_access_token}",
-            "Content-Type": "application/json"
-        }
-        
-        data = {
-        "common": {
-            "transactionId":TransactionID,
-            "uuid": case_uuid 
-        },
-        "paging": {
-            "startRow": 1,
-            "numberOfRows": 100
-        },
-        "caseGetOutput": { 
-            "caseAttributes":{
-            "userFriendlyCaseNumber": True,
-            "title": True,
-            "caseDate": True
+            # Define headers
+            headers = {
+                "Authorization": f"Bearer {KMD_access_token}",
+                "Content-Type": "application/json"
             }
-        }
-        }
-        try:
-            response = requests.put(Caseurl, headers=headers, json=data)
             
-            # Handle response
-            if response.status_code == 200:
-                response_data = response.json()
-                case = response_data["cases"][0]
-
-                # Extract required case attributes
-                title = case["caseAttributes"]["title"]
-                caseDate = case["caseAttributes"]["caseDate"]
-            else: 
-                print("failed to fetch case data")
-        except Exception as e:
-            raise Exception("Failed to fetch case data:", str(e))
-
-
-
-        ## --- API til at opdaterer sagen --- #
-
-        TransactionID = str(uuid.uuid4())
-
-        # Define API URL
-        Caseurl = f"{KMDNovaURL}/Case/Update?api-version=2.0-Case"
-
-        # Define headers
-        headers = {
-            "Authorization": f"Bearer {KMD_access_token}",
-            "Content-Type": "application/json"
-        }
-        data = {
-        "common": {
-        "transactionId": TransactionID,
-        "uuid": case_uuid  
-        },
-        "paging": {
-        "startRow": 1,
-        "numberOfRows": 3000
-        },
-        "caseAttributes": {
-        "title": title,
-        "caseDate": caseDate,
-        "caseCategory": "BomByg"
-        },
-        "state":"Afsluttet",
-        "buildingCase":{
-        "applicationStatusDates":{
-        "decisionDate": datetime.now().strftime("%Y-%m-%dT00:00:00"), # Sættes til den dato hvor afgørelses processen kører
-        "closeDate": datetime.now().strftime("%Y-%m-%dT00:00:00"), # Sættes til den dato hvor afgørelses processen kører
-        "closingReason": "Anden afgørelse"
-        }
-        }
-        }
-        try:
-            response = requests.patch(Caseurl, headers=headers, json=data)
-            
-            # Handle response
-            if response.status_code == 200:
-                print("Sagen er opdateret")
-            else: 
-                raise Exception("failed to fetch case data")
-        except Exception as e:
-            raise Exception("Failed to fetch case data:", str(e))
-
-
-        # --- Henter Task listen --- #
-        Caseurl = f"{KMDNovaURL}/Task/GetList?api-version=2.0-Case"
-        TransactionID = str(uuid.uuid4())
-        # Define headers
-        headers = {
-            "Authorization": f"Bearer {KMD_access_token}",
-            "Content-Type": "application/json"
-        }
-
-        data = {
-        "common": {
-        "transactionId": TransactionID
-        },
-        "paging": {
-        "startRow": 1,
-        "numberOfRows": 3000
-        },
-        "caseUuid": case_uuid, 
-        "taskDescription": True
-        }
-        try:
-            response = requests.put(Caseurl, headers=headers, json=data)
-
-            if response.status_code == 200:
-                print("API call successful. Parsing task list...")
+            data = {
+            "common": {
+                "transactionId":TransactionID,
+                "uuid": case_uuid 
+            },
+            "paging": {
+                "startRow": 1,
+                "numberOfRows": 100
+            },
+            "caseGetOutput": { 
+                "caseAttributes":{
+                "userFriendlyCaseNumber": True,
+                "title": True,
+                "caseDate": True
+                }
+            }
+            }
+            try:
+                response = requests.put(Caseurl, headers=headers, json=data)
                 
-                klar_til_sagsbehandling_uuid = None
-                afslut_sagen_uuid = None
-                tidsreg_sagsbehandling_uuid = None
+                # Handle response
+                if response.status_code == 200:
+                    response_data = response.json()
+                    case = response_data["cases"][0]
 
-                task_list = response.json().get("taskList", [])
-                print(f"Found {len(task_list)} tasks")
-
-                for task in task_list:
-                    title = task.get("taskTitle")
-                    task_uuid = task.get("taskUuid")
-                    print(f"Found task: '{title}' with UUID: {task_uuid}")
-
-                    if title == "05. Klar til sagsbehandling":
-                        klar_til_sagsbehandling_uuid = task_uuid
-                    elif title == "25. Afslut/henlæg sagen":
-                        afslut_sagen_uuid = task_uuid
-                    elif title == "11. Tidsreg: Sagsbehandling":
-                        tidsreg_sagsbehandling_uuid = task_uuid
-
-                # Create a list of tuples with task names and their UUIDs
-                task_uuids = [
-                    ("05. Klar til sagsbehandling", klar_til_sagsbehandling_uuid),
-                    ("25. Afslut/henlæg sagen", afslut_sagen_uuid),
-                    ("11. Tidsreg: Sagsbehandling", tidsreg_sagsbehandling_uuid),
-                ]
-
-                print("\nFinal result:")
-                for task_name, task_uuid in task_uuids:
-                    if task_uuid:
-                        print(f"UUID for '{task_name}': {task_uuid}")
-                    else:
-                        print(f"Missing UUID for task: '{task_name}'")
-            else:
-                print(f"Failed to fetch task data. Status code: {response.status_code}")
-                print(response.text)
-                raise Exception("Failed to fetch task data.")
-
-        except Exception as e:
-            print("Exception occurred:", str(e))
+                    # Extract required case attributes
+                    title = case["caseAttributes"]["title"]
+                    caseDate = case["caseAttributes"]["caseDate"]
+                else: 
+                    print("failed to fetch case data")
+            except Exception as e:
+                raise Exception("Failed to fetch case data:", str(e))
 
 
 
-        # -- Opdaterer Task listen --- #
-        
-        for task_name,task_uuid in task_uuids:
-            Caseurl = f"{KMDNovaURL}/Task/Update?api-version=2.0-Case"
+            ## --- API til at opdaterer sagen --- #
+
+            TransactionID = str(uuid.uuid4())
+
+            # Define API URL
+            Caseurl = f"{KMDNovaURL}/Case/Update?api-version=2.0-Case"
+
+            # Define headers
+            headers = {
+                "Authorization": f"Bearer {KMD_access_token}",
+                "Content-Type": "application/json"
+            }
+            data = {
+            "common": {
+            "transactionId": TransactionID,
+            "uuid": case_uuid  
+            },
+            "paging": {
+            "startRow": 1,
+            "numberOfRows": 3000
+            },
+            "caseAttributes": {
+            "title": title,
+            "caseDate": caseDate,
+            "caseCategory": "BomByg"
+            },
+            "state":"Afsluttet",
+            "buildingCase":{
+            "applicationStatusDates":{
+            "decisionDate": datetime.now().strftime("%Y-%m-%dT00:00:00"), # Sættes til den dato hvor afgørelses processen kører
+            "closeDate": datetime.now().strftime("%Y-%m-%dT00:00:00"), # Sættes til den dato hvor afgørelses processen kører
+            "closingReason": "Anden afgørelse"
+            }
+            }
+            }
+            try:
+                response = requests.patch(Caseurl, headers=headers, json=data)
+                
+                # Handle response
+                if response.status_code == 200:
+                    print("Sagen er opdateret")
+                else: 
+                    raise Exception("failed to fetch case data")
+            except Exception as e:
+                raise Exception("Failed to fetch case data:", str(e))
+
+
+            # --- Henter Task listen --- #
+            Caseurl = f"{KMDNovaURL}/Task/GetList?api-version=2.0-Case"
             TransactionID = str(uuid.uuid4())
             # Define headers
             headers = {
-            "Authorization": f"Bearer {KMD_access_token}",
-            "Content-Type": "application/json"
+                "Authorization": f"Bearer {KMD_access_token}",
+                "Content-Type": "application/json"
             }
 
-            task_data= {
+            data = {
             "common": {
-                "transactionId": TransactionID
+            "transactionId": TransactionID
             },
-            "uuid": task_uuid, 
-            "caseUuid": case_uuid,
-            "title": task_name, 
-            "caseworker": { 
-                "kspIdentity": {
-                    "novaUserId": "78897bfc-2a36-496d-bc76-07e7a6b0850e",
-                    "racfId": "AZX0075",
-                    "fullName": "Aktindsigter Novabyg"
-                }
+            "paging": {
+            "startRow": 1,
+            "numberOfRows": 3000
             },
-            "closeDate": task_date,
-            "statusCode": "F",
-            "taskType": "Aktivitet" 
+            "caseUuid": case_uuid, 
+            "taskDescription": True
             }
-            
             try:
-                response = requests.put(Caseurl, headers=headers, json=task_data)
+                response = requests.put(Caseurl, headers=headers, json=data)
+
                 if response.status_code == 200:
-                    print(f"{task_name} er blevet færdiggjort")
-                else: 
-                    print(response.status_code)
+                    print("API call successful. Parsing task list...")
+                    
+                    klar_til_sagsbehandling_uuid = None
+                    afslut_sagen_uuid = None
+                    tidsreg_sagsbehandling_uuid = None
+
+                    task_list = response.json().get("taskList", [])
+                    print(f"Found {len(task_list)} tasks")
+
+                    for task in task_list:
+                        title = task.get("taskTitle")
+                        task_uuid = task.get("taskUuid")
+                        print(f"Found task: '{title}' with UUID: {task_uuid}")
+
+                        if title == "05. Klar til sagsbehandling":
+                            klar_til_sagsbehandling_uuid = task_uuid
+                        elif title == "25. Afslut/henlæg sagen":
+                            afslut_sagen_uuid = task_uuid
+                        elif title == "11. Tidsreg: Sagsbehandling":
+                            tidsreg_sagsbehandling_uuid = task_uuid
+
+                    # Create a list of tuples with task names and their UUIDs
+                    task_uuids = [
+                        ("05. Klar til sagsbehandling", klar_til_sagsbehandling_uuid),
+                        ("25. Afslut/henlæg sagen", afslut_sagen_uuid),
+                        ("11. Tidsreg: Sagsbehandling", tidsreg_sagsbehandling_uuid),
+                    ]
+
+                    print("\nFinal result:")
+                    for task_name, task_uuid in task_uuids:
+                        if task_uuid:
+                            print(f"UUID for '{task_name}': {task_uuid}")
+                        else:
+                            print(f"Missing UUID for task: '{task_name}'")
+                else:
+                    print(f"Failed to fetch task data. Status code: {response.status_code}")
                     print(response.text)
+                    raise Exception("Failed to fetch task data.")
+
             except Exception as e:
-                raise Exception("Failed to update task:", str(e))
+                print("Exception occurred:", str(e))
+
+
+
+            # -- Opdaterer Task listen --- #
+            
+            for task_name,task_uuid in task_uuids:
+                Caseurl = f"{KMDNovaURL}/Task/Update?api-version=2.0-Case"
+                TransactionID = str(uuid.uuid4())
+                # Define headers
+                headers = {
+                "Authorization": f"Bearer {KMD_access_token}",
+                "Content-Type": "application/json"
+                }
+
+                task_data= {
+                "common": {
+                    "transactionId": TransactionID
+                },
+                "uuid": task_uuid, 
+                "caseUuid": case_uuid,
+                "title": task_name, 
+                "caseworker": { 
+                    "kspIdentity": {
+                        "novaUserId": "78897bfc-2a36-496d-bc76-07e7a6b0850e",
+                        "racfId": "AZX0075",
+                        "fullName": "Aktindsigter Novabyg"
+                    }
+                },
+                "closeDate": task_date,
+                "statusCode": "F",
+                "taskType": "Aktivitet" 
+                }
+                
+                try:
+                    response = requests.put(Caseurl, headers=headers, json=task_data)
+                    if response.status_code == 200:
+                        print(f"{task_name} er blevet færdiggjort")
+                    else: 
+                        print(response.status_code)
+                        print(response.text)
+                except Exception as e:
+                    raise Exception("Failed to update task:", str(e))
 
     return {
     "out_Text": "Sagen er afsluttet"
